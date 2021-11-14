9c00f9e57819a78119c8d397d35e8a31e9091512
Arya Krishna <dr.aryakris@outlook.com>
2021-11-14 11:48:22 -0800

    created prompt for the length, charecterlists, and confirm for numbers

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,30 @@
+var numbersList = [0,1,2,3,4,5,6,7,8,9];
+var uppercaseList = ["A","B", "C","D", "E"];
+var lowercaseList = [ "a", "b", "c", "d", "e"];
+var specialList = ["!", "@", "#", "$", "%"];
+var totalList = [];
 
+var generatePassword = function() {
+var passwordLength= prompt("How long is the password?")
+console.log (passwordLength);
+  if(passwordLength < 8) {
+    alert("value of the password length should be atleast 8")
+    return "Password is too short"
+  }
+  
+    else if (passwordLength > 128) {
+  alert("value of the password should not be more than 128")
+  return "password is too long"
+    }
+  
+var numbersTrue = confirm("Do you want numbers in your password?")
+if (numbersTrue === true) {
+totalList =totalList.concat (numbersList);
+// var item = randomize(numbersList);
+}
+
+return "password";
+}
 
 
 
